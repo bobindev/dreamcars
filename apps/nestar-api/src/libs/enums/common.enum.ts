96b02ec93cf311472ab41bc446369bd448545583
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-05-08 16:58:10 +0900

    feat: build project standards

---
+++ apps/nestar-api/src/libs/enums/common.enum.ts
@@ -0,0 +1,17 @@
+export enum Message {
+  SOMETHING_WENT_WRONG = 'Somwthing went wrong!',
+  NO_DATA_FOUND = 'No data found!',
+  CREATE_FAILED = 'Create failed!',
+  UPDATE_FAILED = 'Update failed!',
+  BAD_REQUEST = 'Bad request!',
+
+  NO_MEMBER_NICK = 'No member with that member nick',
+  BLOCKED_USER = 'You have been blocked!',
+  WRONG_PASSWORD = 'Wrong password, try again!',
+  NOT_AUTHENTICATED = 'You are not authenticated, please login first!',
+  TOKEN_NOT_EXIST = 'Bearer Token is not provided!',
+  ONLY_SPECIFIC_ROLES_ALLOWED = 'Allowed only for members with specific roles!',
+  NOT_ALLOWED_REQUEST = 'Not Allowed Request!',
+  PROVIDE_ALLOWED_FORMAT = 'Please provide jpg, jpeg, or pnp images!',
+  SELF_SUBSCRIPTION_DENIED = 'Self subscribtion is denied!',
+}
