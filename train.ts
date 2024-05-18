ce70a6f554f624132da16245bec3f5387aeebbfc
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-05-18 17:56:13 +0900

    feat: ZL-TASK is done

--- train.ts
+++ train.ts
@@ -1,11 +1,18 @@
 //This is TRAIN ARIA
 console.log('TRAIN ARIA:');
 
-//ZM-TASK
+//ZL -TASK
 
-function reverseInteger(Int: number) {
-	let reverse = Int.toString().split('').reverse().join('');
-	return reverse;
+
+function rotateArray(arr: string | any[], index: number) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i++) {
+      if (i <= index) {
+          newArr.push(arr[i]);
+      } else if (i > index) {
+          newArr.unshift(arr[i]);
+      }
+  } return newArr;
 }
 
-console.log(reverseInteger(123456789));
+console.log(rotateArray([1, 2, 3, 4, 5, 6], 3) );
