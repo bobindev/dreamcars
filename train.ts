05029c739beb052f5c6933768a12169d49599b8d
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-05-28 15:51:24 +0900

    feat: ZR-TASK is done

--- train.ts
+++ train.ts
@@ -1,27 +1,20 @@
 //This is TRAIN ARIA
 console.log('TRAIN ARIA:');
 
-//ZP-TASK
+//ZR-TASK
 
-function majorityElement(arr) {
-  let mf = 1;
-  let m = 0;
-  let item;
-
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = i; j < arr.length; j++) {
-      if (arr[i] == arr[j]) {
-        m++;
-        if (m > mf) {
-          mf = m;
-          item = arr[i];
-        }
-      }
-    }
-    m = 0;
-  }
-
-  return item;
+function countNumberAndLetters(str) {
+	let count = {
+		number: 0,
+		letter: 0,
+	};
+	for (let i = 0; i < str.length; i++) {
+		if (str[i] >= 0 && str[i] <= 9) {
+			count.number++;
+		} else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
+			count.letter++;
+		}
+	}
+	return count;
 }
-
-console.log( majorityElement([1,2,3,4,5,4,3,4]))
+console.log(countNumberAndLetters('string152%¥'));
