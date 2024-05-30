dea60b8b6cb5b99767e471f4f8b70e08c79792d2
boburasatullaev91@gmail.com <boburasatullaev91@gmail.com>
2024-05-30 16:45:57 +0900

    feat: ZS-TASK is done

--- train.ts
+++ train.ts
@@ -1,20 +1,14 @@
 //This is TRAIN ARIA
 console.log('TRAIN ARIA:');
 
-//ZR-TASK
+//ZS-TASK
 
-function countNumberAndLetters(str) {
-	let count = {
-		number: 0,
-		letter: 0,
-	};
-	for (let i = 0; i < str.length; i++) {
-		if (str[i] >= 0 && str[i] <= 9) {
-			count.number++;
-		} else if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) {
-			count.letter++;
-		}
-	}
-	return count;
+function singleNumber(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    result ^= arr[i];
+  }
+  return result;
 }
-console.log(countNumberAndLetters('string152%¥'));
+
+console.log(singleNumber([4, 2, 1, 2, 1]));
